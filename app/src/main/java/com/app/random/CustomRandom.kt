4fec98968a7921c4658b7b817ef98521dd7a9ee2
prasannakumarboddeda <prasannakumarboddeda@gmail.com>
2023-02-02 21:39:06 +0530

    commit

--- app/src/main/java/com/app/random/CustomRandom.kt
+++ app/src/main/java/com/app/random/CustomRandom.kt
@@ -3,6 +3,7 @@ package com.app.random
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.random.databinding.ActivityCustomRandomBinding
 
@@ -19,11 +20,13 @@ class CustomRandom : AppCompatActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         list = ArrayList()
         binding.recyclerView.adapter=ListAdapter(list)
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(this,DividerItemDecoration.VERTICAL))
         binding.addBtn.setOnClickListener {
             list.add(Data(binding.editText.text.toString()))
             Toast.makeText(this,"Added "+list[list.size-1].item,Toast.LENGTH_SHORT).show()
             binding.recyclerView.adapter=ListAdapter(list)
         }
+
 
         binding.getBtn.setOnClickListener {
             binding.show.text = list[(0 until list.size-1).random()].item
